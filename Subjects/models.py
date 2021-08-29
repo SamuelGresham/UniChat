50ec76c1a368a099e1bc75a0934a4a6f96837e6f
samuelgresham12 <samuelgresham12@gmail.com>
2021-08-29 17:28:36 +1000

    Database

--- Subjects/models.py
+++ Subjects/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Subject (models.Model):
+    subject_code = models.CharField(max_length=10, primary_key=True)
+    subject_name = models.CharField(max_length=50)
+    faculty_name = models.CharField(max_length=50)
+
+class Chat (models.Model):
+    chat_platform = models.CharField(max_length=50)
+    chat_link = models.CharField(max_length=100)
